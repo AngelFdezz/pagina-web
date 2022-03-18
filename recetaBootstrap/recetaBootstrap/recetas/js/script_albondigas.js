0411f47d452115b341631a2f877e8300dd2f0f54
AngelFdezz <91657087+AngelFdezz@users.noreply.github.com>
2022-03-18 12:06:39 +0100

    Add files via upload

---
+++ recetaBootstrap/recetaBootstrap/recetas/js/script_albondigas.js
@@ -0,0 +1,16 @@
+var inicialText = $('.ingredientList').html()
+console.log(inicialText)
+
+function myFunction() {
+    if ($('.ingredientList').html() === inicialText) {
+        $( ".ingredientList'" ).fadeIn( "slow", function() {
+            $('.ingredientList').html("<ol> <li> Juntar los ingredientes para la elaboración de masa de albóndigas en un bol amplio. Con las manos mezclar hasta integrar </li> <li> Una vez la masa adquiera una textura compacta empezar a formar bolitas de tamaño regular </li> <li>Reservar en una fuente según se formen </li> <li> En una cazuela con AOVE cocinar hasta que estén doradas en su exterior. Reservar en papel absorbente </li> <li>Añadir las almendras y tostar. Retirar del fuego </li> <li> Verter el vino blanco y evaporar el alcohol </li>  </ol>");
+            $('h3').html("Cómo hacer")
+          });
+ 
+    } else {
+        $('.ingredientList').html(inicialText).fadeIn('slow');
+        $('h3').html("Ingredientes").fadeIn('slow')
+
+    }
+  }
